04f600b6f055c6ca5cfb53a643c4f8e54c8cbc0b
Abhishekwmg <ak89471@gmail.com>
2026-04-08 23:47:13 +0530

    weather-cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { getWeahter } from "./api";
+import Card from "./components/cards/Cards";
 
 function App() {
   const { data } = useQuery({
@@ -7,7 +8,19 @@ function App() {
     queryFn: () => getWeahter({ lat: 50, lon: 10 }),
   });
 
-  return <>{JSON.stringify(data)}</>;
+  return (
+    <div className="flex flex-col gap-8">
+      <Card title="Current Weather">
+        {JSON.stringify(data?.current).slice(0, 100)}
+      </Card>
+      <Card title="Hourly Forecast (48 Hours)">
+        {JSON.stringify(data?.hourly).slice(0, 100)}
+      </Card>
+      <Card title="Daily Forecast">
+        {JSON.stringify(data?.daily).slice(0, 100)}
+      </Card>
+    </div>
+  );
 }
 
 export default App;
