06970350d63e6f6c1abba48fcca72f7fce4d39db
Abhishekwmg <ak89471@gmail.com>
2026-04-12 16:24:44 +0530

    curr_weather_card

--- src/components/cards/CurrentWeather.tsx
+++ src/components/cards/CurrentWeather.tsx
@@ -1,6 +1,7 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { getWeahter } from "../../api";
 import Card from "./Cards";
+import WeatherIcon from "../WeatherIcon";
 
 export default function CurrentWeather() {
   const { data } = useSuspenseQuery({
@@ -9,8 +10,16 @@ export default function CurrentWeather() {
   });
 
   return (
-    <Card title="Current Weather" childrenClassName="flex flex-col">
-      <div className="">{Math.round(data.current.temp)}°F</div>
+    <Card
+      title="Current Weather"
+      childrenClassName="flex flex-col items-center"
+    >
+      <div className="flex flex-col gap-2 items-center">
+        <h2 className="text-6xl font-semibold text-center">
+          {Math.round(data.current.temp)}°F
+        </h2>
+        <WeatherIcon src={data.current.weather[0].icon} className="size-14" />
+      </div>
     </Card>
   );
 }
