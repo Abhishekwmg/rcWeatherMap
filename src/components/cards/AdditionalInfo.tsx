8bdbdf4683cf6d98945155c2fbc082ae551802c0
Abhishekwmg <ak89471@gmail.com>
2026-04-13 23:39:29 +0530

    additional-info-card

---
+++ src/components/cards/AdditionalInfo.tsx
@@ -0,0 +1,88 @@
+import Card from "./Cards";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { getWeahter } from "../../api";
+
+import Cloudiness from "../../../src/assets/cloudiness.svg?react";
+import Pressure from "../../../src/assets/pressure.svg?react";
+import Sunrise from "../../../src/assets/sunrise.svg?react";
+import Sunset from "../../../src/assets/sunset.svg?react";
+import UV from "../../../src/assets/uv.svg?react";
+import Wind from "../../../src/assets/wind.svg?react";
+import UpArrow from "../../../src/assets/arrow-up.svg?react";
+
+export default function AdditionalInfo() {
+  const { data } = useSuspenseQuery({
+    queryKey: ["weather"],
+    queryFn: () => getWeahter({ lat: 28, lon: 77 }),
+  });
+
+  return (
+    <Card
+      title="Additional Weather Info"
+      childrenClassName="flex flex-col gap-8"
+    >
+      {rows.map(({ label, value, Icon }) => (
+        <div className="flex justify-between" key={value}>
+          <div className="flex gap-4">
+            <span className="text-gray-500">{label}</span>
+            <Icon className="size-8 invert-50" />
+          </div>
+          <span>
+            <FormatComponent value={value} number={data.current[value]} />
+          </span>
+        </div>
+      ))}
+    </Card>
+  );
+}
+
+function FormatComponent({ value, number }: { value: string; number: number }) {
+  if (value === "sunrise" || value === "sunset")
+    return new Date(number * 1000).toLocaleTimeString(undefined, {
+      hour: "numeric",
+      minute: "2-digit",
+      hour12: true,
+    });
+
+  if (value === "wind_deg")
+    return (
+      <UpArrow
+        className="size-8 invert-50"
+        style={{ transform: `rotate(${number}deg)` }}
+      />
+    );
+  return number;
+}
+
+const rows = [
+  {
+    label: "Cloudiness (%)",
+    value: "clouds",
+    Icon: Cloudiness,
+  },
+  {
+    label: "UV Index",
+    value: "uvi",
+    Icon: UV,
+  },
+  {
+    label: "Wind Direction",
+    value: "wind_deg",
+    Icon: Wind,
+  },
+  {
+    label: "Pressure (hPa)",
+    value: "pressure",
+    Icon: Pressure,
+  },
+  {
+    label: "Sunrise",
+    value: "sunrise",
+    Icon: Sunrise,
+  },
+  {
+    label: "Sunset",
+    value: "sunset",
+    Icon: Sunset,
+  },
+] as const;
--- src/components/cards/CurrentWeather.tsx
+++ src/components/cards/CurrentWeather.tsx
@@ -12,13 +12,41 @@ export default function CurrentWeather() {
   return (
     <Card
       title="Current Weather"
-      childrenClassName="flex flex-col items-center"
+      childrenClassName="flex flex-col items-center gap-4"
     >
       <div className="flex flex-col gap-2 items-center">
         <h2 className="text-6xl font-semibold text-center">
           {Math.round(data.current.temp)}°F
         </h2>
         <WeatherIcon src={data.current.weather[0].icon} className="size-14" />
+        <h3 className="capitalize text-xl">
+          {data.current.weather[0].description}
+        </h3>
+      </div>
+      <div className="flex flex-col gap2">
+        <p className="text-xl text-center">Local Time</p>
+        <h3 className="text-4xl font-semibold">
+          {new Intl.DateTimeFormat("en-US", {
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: true,
+            timeZone: data.timezone,
+          }).format(new Date(data.current.dt * 1000))}
+        </h3>
+      </div>
+      <div className="flex justify-between w-full">
+        <div className="flex flex-col items-center gap-2">
+          <p className="text-gray-500">Feels Like</p>
+          <p>{Math.round(data.current.feels_like)}°F</p>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <p className="text-gray-500">Humidity</p>
+          <p>{Math.round(data.current.humidity)}%</p>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <p className="text-gray-500">Wind</p>
+          <p>{data.current.wind_speed}mph</p>
+        </div>
       </div>
     </Card>
   );
