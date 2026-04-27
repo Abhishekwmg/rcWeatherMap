d61f79814d765e189d993f6a98aef858220709ae
Abhishekwmg <ak89471@gmail.com>
2026-04-28 01:40:07 +0530

    build-fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./App/index.css";
+import "./app/index.css";
 import App from "./app/App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ThemeProvider from "./context/ThemeProvider.tsx";
